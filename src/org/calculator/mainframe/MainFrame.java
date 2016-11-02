1ecece0519c8156c28734800331d5e3871831b02
Nojahhh <waxnsax@gmail.com>
2016-11-02 11:43:14 +0100

    Rebase NRButtons

--- src/org/calculator/mainframe/MainFrame.java
+++ src/org/calculator/mainframe/MainFrame.java
@@ -11,22 +11,20 @@ import javax.swing.SwingConstants;
 
 import org.calculator.classes.BasicMethods;
 import javax.swing.JLabel;
+import java.awt.Font;
 
 public class MainFrame implements ActionListener {
 
-	private BasicMethods bm = new BasicMethods();
 	private JFrame frame;
-	
 	private JButton btnAddition = new JButton("+");
 	private JButton btnSubtraction = new JButton("-");
 	private JButton btnMultiplication = new JButton("X");
 	private JButton btnDivision = new JButton("/");
-	private JButton btnClear = new JButton("Clear");
-	
-	private JFormattedTextField frmtdtxtInputNum1 = new JFormattedTextField();
-	private JFormattedTextField frmtdtxtInputNum2 = new JFormattedTextField();
+	private JButton btnClear = new JButton("C");
+	private BasicMethods bm = new BasicMethods();
+	private JFormattedTextField frmtdtxtfldNum = new JFormattedTextField();
+	private JFormattedTextField frmtdtxtfldNum_1 = new JFormattedTextField();
 	private JFormattedTextField frmtdtxtfldResult = new JFormattedTextField();
-
 	private JLabel lblResultLabel = new JLabel("Result: ");
 	private JLabel lblth = new JLabel("1st:");
 	private JLabel lblSecondNumber = new JLabel("2nd:");
@@ -48,7 +46,6 @@ public class MainFrame implements ActionListener {
 	private final JButton buttonComma = new JButton(",");
 	private final JButton button0 = new JButton("0");
 	private final JButton buttonEquals = new JButton("=");
-
 	/**
 	 * Create the application.
 	 */
@@ -61,51 +58,77 @@ public class MainFrame implements ActionListener {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 500, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		btnAddition.setBounds(420, 339, 50, 25);
 		frame.getContentPane().add(btnAddition);
+		
+
+		btnSubtraction.setBounds(420, 301, 50, 25);
 		frame.getContentPane().add(btnSubtraction);
 
 		btnMultiplication.setBounds(420, 263, 50, 25);
-		
 		frame.getContentPane().add(btnMultiplication);
-		frame.getContentPane().add(btnDivision);
-		frame.getContentPane().add(frmtdtxtInputNum1);
-		frame.getContentPane().add(frmtdtxtInputNum2);
-		frame.getContentPane().add(frmtdtxtfldResult);
-		frame.getContentPane().add(lblResultLabel);
-		frame.getContentPane().add(lblSecondNumber);
-		frame.getContentPane().add(lblth);
-		frame.getContentPane().add(btnClear);
-		frame.setVisible(true);
 		
-		btnAddition.setBounds(420, 339, 50, 25);
-		btnSubtraction.setBounds(420, 301, 50, 25);
-		btnMultiplication.setBounds(420, 263, 50, 25);
+
 		btnDivision.setBounds(420, 225, 50, 25);
-		btnClear.setBounds(373, 415, 97, 25);
+		frame.getContentPane().add(btnDivision);
 		
-		frmtdtxtInputNum1.setText("");
-		frmtdtxtInputNum1.setBounds(70, 50, 360, 30);
-			
-		frmtdtxtInputNum2.setText("");
-		frmtdtxtInputNum2.setBounds(70, 100, 360, 30);
+
+		frmtdtxtfldNum.setText("");
+		frmtdtxtfldNum.setBounds(70, 50, 360, 30);
+		frame.getContentPane().add(frmtdtxtfldNum);
 		
+		frmtdtxtfldNum_1.setText("");
+		frmtdtxtfldNum_1.setBounds(70, 100, 360, 30);
+		frame.getContentPane().add(frmtdtxtfldNum_1);
 		frmtdtxtfldResult.setText("");
 		frmtdtxtfldResult.setBounds(70, 143, 360, 30);
 		
-		
+		frame.getContentPane().add(frmtdtxtfldResult);
 		lblResultLabel.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblResultLabel.setBounds(12, 143, 50, 30);
 		
+		frame.getContentPane().add(lblResultLabel);
+		
+
 		lblSecondNumber.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblSecondNumber.setBounds(2, 107, 56, 16);
+		frame.getContentPane().add(lblSecondNumber);
 		
+
 		lblth.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblth.setBounds(6, 57, 56, 16);
+		frame.getContentPane().add(lblth);
+		
+
+		btnClear.setBounds(291, 339, 60, 25);
+		frame.getContentPane().add(btnClear);
+		btnPi.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		btnPi.setBounds(12, 225, 50, 25);
+		
+		frame.getContentPane().add(btnPi);
+		btnExp.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		btnExp.setBounds(12, 263, 50, 25);
+		
+		frame.getContentPane().add(btnExp);
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		btnNewButton.setBounds(12, 301, 50, 25);
+		
+		frame.getContentPane().add(btnNewButton);
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		btnNewButton_1.setBounds(69, 225, 50, 25);
+		
+		frame.getContentPane().add(btnNewButton_1);
+		btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		btnNewButton_2.setBounds(69, 263, 50, 25);
+		
+		frame.getContentPane().add(btnNewButton_2);
+		btnNewButton_3.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		btnNewButton_3.setBounds(69, 301, 50, 25);
 		
 		frame.getContentPane().add(btnNewButton_3);
 		button1.setBounds(147, 225, 60, 25);
@@ -145,9 +168,6 @@ public class MainFrame implements ActionListener {
 		
 		frame.getContentPane().add(buttonEquals);
 		frame.setVisible(true);
-
-		
-		
 	}
 	public void addActionListener() {
 		btnAddition.addActionListener(this);
@@ -171,10 +191,10 @@ public class MainFrame implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		/*if (e.getSource() == btnAddition) {
+	/*	if (e.getSource() == btnAddition) {
 			try {
-			String firstField = frmtdtxtInputNum1.getText();
-			String secondField = frmtdtxtInputNum2.getText();
+			String firstField = frmtdtxtfldNum.getText();
+			String secondField = frmtdtxtfldNum_1.getText();
 			double txt2dbl1 = Double.parseDouble(firstField);
 			double txt2dbl2 = Double.parseDouble(secondField);
 			double res = bm.add(txt2dbl1, txt2dbl2);
@@ -185,8 +205,8 @@ public class MainFrame implements ActionListener {
 		}
 		if (e.getSource() == btnSubtraction) {
 			try {
-			String firstField = frmtdtxtInputNum1.getText();
-			String secondField = frmtdtxtInputNum2.getText();
+			String firstField = frmtdtxtfldNum.getText();
+			String secondField = frmtdtxtfldNum_1.getText();
 			double txt2dbl1 = Double.parseDouble(firstField);
 			double txt2dbl2 = Double.parseDouble(secondField);
 			double res = bm.subtract(txt2dbl1, txt2dbl2);
@@ -197,8 +217,8 @@ public class MainFrame implements ActionListener {
 		}
 		if (e.getSource() == btnMultiplication) {
 			try {
-			String firstField = frmtdtxtInputNum1.getText();
-			String secondField = frmtdtxtInputNum2.getText();
+			String firstField = frmtdtxtfldNum.getText();
+			String secondField = frmtdtxtfldNum_1.getText();
 			double txt2dbl1 = Double.parseDouble(firstField);
 			double txt2dbl2 = Double.parseDouble(secondField);
 			double res = bm.multiplication(txt2dbl1, txt2dbl2);
@@ -209,8 +229,8 @@ public class MainFrame implements ActionListener {
 		}
 		if (e.getSource() == btnDivision) {
 			try {
-			String firstField = frmtdtxtInputNum1.getText();
-			String secondField = frmtdtxtInputNum2.getText();
+			String firstField = frmtdtxtfldNum.getText();
+			String secondField = frmtdtxtfldNum_1.getText();
 			double txt2dbl1 = Double.parseDouble(firstField);
 			double txt2dbl2 = Double.parseDouble(secondField);
 			double res = bm.division(txt2dbl1, txt2dbl2);
@@ -221,65 +241,63 @@ public class MainFrame implements ActionListener {
 		}
 		if (e.getSource() == btnClear) {
 			try {
-				frmtdtxtInputNum1.setText("");
-				frmtdtxtInputNum2.setText("");
+				frmtdtxtfldNum.setText("");
+				frmtdtxtfldNum_1.setText("");
 				frmtdtxtfldResult.setText("");
 			} catch (Exception a) {
 				a.getMessage();
 			}
-
 		}*/
 		if (e.getSource() == btnAddition) {
-			frmtdtxtInputNum1.setText(frmtdtxtInputNum1.getText()+"+");
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+"+");
 		}
 		if (e.getSource() == btnSubtraction) {
-			frmtdtxtInputNum1.setText(frmtdtxtInputNum1.getText()+"-");
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+"-");
 		}
 		if (e.getSource() == btnMultiplication) {
-			frmtdtxtInputNum1.setText(frmtdtxtInputNum1.getText()+"*");
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+"*");
 		}
 		if (e.getSource() == btnDivision) {
-			frmtdtxtInputNum1.setText(frmtdtxtInputNum1.getText()+"/");
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+"/");
 		}
 		if (e.getSource() == button1) {
-			frmtdtxtInputNum1.setText(frmtdtxtInputNum1.getText()+"1");
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+"1");
 		}
 		if (e.getSource() == button2) {
-			frmtdtxtInputNum1.setText(frmtdtxtInputNum1.getText()+"2");
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+"2");
 		}
 		if (e.getSource() == button3) {
-			frmtdtxtInputNum1.setText(frmtdtxtInputNum1.getText()+"3");
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+"3");
 		}
 		if (e.getSource() == button4) {
-			frmtdtxtInputNum1.setText(frmtdtxtInputNum1.getText()+"4");
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+"4");
 		}
 		if (e.getSource() == button5) {
-			frmtdtxtInputNum1.setText(frmtdtxtInputNum1.getText()+"5");
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+"5");
 		}
 		if (e.getSource() == button6) {
-			frmtdtxtInputNum1.setText(frmtdtxtInputNum1.getText()+"6");
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+"6");
 		}
 		if (e.getSource() == button7) {
-			frmtdtxtInputNum1.setText(frmtdtxtInputNum1.getText()+"7");
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+"7");
 		}
 		if (e.getSource() == button8) {
-			frmtdtxtInputNum1.setText(frmtdtxtInputNum1.getText()+"8");
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+"8");
 		}
 		if (e.getSource() == button9) {
-			frmtdtxtInputNum1.setText(frmtdtxtInputNum1.getText()+"9");
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+"9");
 		}
 		if (e.getSource() == button0) {
-			frmtdtxtInputNum1.setText(frmtdtxtInputNum1.getText()+"0");
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+"0");
 		}
 		if (e.getSource() == buttonComma) {
-			frmtdtxtInputNum1.setText(frmtdtxtInputNum1.getText()+",");
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+",");
 		}
 		if (e.getSource() == btnClear) {
-			frmtdtxtInputNum1.setText("");
+			frmtdtxtfldNum.setText("");
 		}
 		if (e.getSource() == buttonEquals) {
-			frmtdtxtInputNum1.getText();
-
+			frmtdtxtfldNum.setText(frmtdtxtfldNum.getText()+"1");
 		}
 		
 	}
