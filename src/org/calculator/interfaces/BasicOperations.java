11c00270a21202003469b8044b3915c63b8f5dec
Mattias Svensson <matte_sesso@hotmail.com>
2016-11-07 15:44:35 +0100

    Just some more comments

--- src/org/calculator/interfaces/BasicOperations.java
+++ src/org/calculator/interfaces/BasicOperations.java
@@ -1,5 +1,12 @@
 package org.calculator.interfaces;
-
+/**
+ * 
+ * @author Johan Melin
+ *
+ */
+/**
+ * Interface for basic operations that we want to implement
+ */
 public interface BasicOperations {
 	public double add(double firstNumber, double secondNumber);
 	public double subtract(double firstNumber, double secondNumber);
